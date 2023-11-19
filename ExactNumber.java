1e843a015eb6628ef9bc216ba845d38fe43a61af
KAMIKOTOPO <daniilseluha51@gmail.com>
2023-11-19 12:53:56 +0300

    Добавил сложение и вычитание(немного не доделал)

--- ExactNumber.java
+++ ExactNumber.java
@@ -2,52 +2,160 @@ package lab_07;
 
 import java.util.Arrays;
 
+
 class ExactNumber {
 	private byte[] mantissa;
 	private int order;
 	private int signMantissa;
-	private int signOrder;
-	
-	public void readNumber() {
+	private int numberLength;
+	private final int numberZeroInBytes = 48;
+
+	public ExactNumber() {
+		this.mantissa = new byte[30];
 	}
-	
-	public void setFigureNumber(int i, byte figure){
-		this.mantissa[i] = figure;
+
+	public boolean checkOrder() {
+		if (-5 > order && 5 < order) {
+			System.out.println("Порядок числа меньше либо больше допустимо");
+			return false;
+		} else {
+			return true;
+		}
 	}
-	
-	public byte getFigureNumber(int i) {
+
+	public void addNumber(String stringNumber) {
+		order = 0;
+		numberLength = 0;
+		char[] charNumber = stringNumber.toCharArray();
+		int mantissaLength = charNumber.length;
+		int index = 0;
+		if (charNumber[0] == '-') {
+			signMantissa = -1;
+		} else if (charNumber[0] == '+' || Character.isDigit(charNumber[0])) {
+			signMantissa = 1;
+		}
+		if (stringNumber.contains(".")) {
+			int indexPoint = stringNumber.indexOf(".");
+			if (Integer.parseInt(stringNumber.substring(0, indexPoint)) == 0) {
+				order = -1;
+				for (int i = indexPoint + 1; i < mantissaLength; i++) {
+					if (charNumber[i] != '0') {
+						break;
+					}
+					order--;
+				}
+			} else {
+				for (int i = 0; i < charNumber.length; i++) {
+					if (charNumber[i] == '.') {
+						break;
+					} else if (Character.isDigit(charNumber[i])) {
+						order++;
+					}
+				}
+			}
+		}
+		if (stringNumber.contains("e")) {
+			mantissaLength = stringNumber.indexOf("e");
+			if (mantissaLength < charNumber.length) {
+				order += Integer.parseInt(stringNumber.substring(mantissaLength + 1, charNumber.length));
+			}
+		}
+		for (int i = 0; i < mantissaLength; i++) {
+			if (Character.isDigit(charNumber[i])) {
+				mantissa[index] = (byte) charNumber[i];
+				index++;
+				numberLength++;
+			}
+		}
+	}
+
+	private byte getElementArrayMantisa(int i) {
 		return mantissa[i];
 	}
-	
-	public void setSignMantissa(int sign) {
-		this.signMantissa = sign;
+
+	public void addition(ExactNumber secondExactNumber) {
+		int differentOrder = Math.abs(order - secondExactNumber.order);
+		int ten = 0;
+		for (int i = mantissa.length - 1; i >= 0; i--) {
+			byte sum = 0;
+			if (i >= differentOrder) {
+				if (order < secondExactNumber.order) {
+					sum = (byte) (signMantissa * mantissa[i - differentOrder] + (secondExactNumber.signMantissa * secondExactNumber.mantissa[i] + ten));
+				} else if (order > secondExactNumber.order) {
+					sum = (byte) (signMantissa*mantissa[i] + (secondExactNumber.signMantissa *secondExactNumber.mantissa[i - differentOrder] + ten));
+				} else if (order == secondExactNumber.order) {
+					sum = (byte) (signMantissa *mantissa[i] + (secondExactNumber.signMantissa *secondExactNumber.mantissa[i] + ten));
+				}
+				if (sum >= 110) {
+					ten++;
+				}
+				secondExactNumber.mantissa[i] = sum;
+			} else {
+				if (order < secondExactNumber.order) {
+					secondExactNumber.mantissa[i] = (byte) (secondExactNumber.mantissa[i] + ten);
+				} else if (order > secondExactNumber.order) {
+					secondExactNumber.mantissa[i] = (byte) (mantissa[i] + ten);
+				}
+			}
+			ten = 0;
+		}
+		secondExactNumber.order = Math.max(order, secondExactNumber.order);
 	}
 	
-	public byte[] getMantissa() {
-		return mantissa;
+	public void subtraction(ExactNumber secondExactNumber) {
+		int differentOrder = Math.abs(order - secondExactNumber.order);
+		int minusOrder = 0;
+		for (int i = mantissa.length - 1; i >= 0; i--) {
+			int sum = 0;
+			if (i >= differentOrder) {
+				if (order < secondExactNumber.order) {
+					int firstNumber = (signMantissa* (mantissa[i - differentOrder]-numberZeroInBytes));
+					int secondNumber = (secondExactNumber.signMantissa * (secondExactNumber.mantissa[i]-numberZeroInBytes));
+					if((signMantissa == 1 && secondExactNumber.signMantissa == 1&& mantissa[i]<secondExactNumber.mantissa[i])) {
+						firstNumber += 10;
+					}
+					if(signMantissa == -1 && secondExactNumber.signMantissa == -1&& mantissa[i]>secondExactNumber.mantissa[i]){
+						secondNumber += 10;
+					}
+					sum = firstNumber - secondNumber;
+				} else if (order > secondExactNumber.order) {
+					sum = (signMantissa * (mantissa[i]-numberZeroInBytes) ) -(secondExactNumber.signMantissa * (secondExactNumber.mantissa[i - differentOrder]-numberZeroInBytes));
+				} else if (order == secondExactNumber.order) {
+					sum = (signMantissa*(mantissa[i ]-numberZeroInBytes)) - (secondExactNumber.signMantissa * (secondExactNumber.mantissa[i]-numberZeroInBytes));
+				}
+				System.out.println(sum);
+				if(sum > 10 ) {
+					sum--;
+					mantissa[i-differentOrder-1]++;
+				}
+				secondExactNumber.mantissa[i] = (byte) ((char)(sum)+'0');
+			} else {
+				if (order < secondExactNumber.order) {
+					secondExactNumber.mantissa[i] = (byte) (secondExactNumber.mantissa[i]-numberZeroInBytes);
+				} else if (order > secondExactNumber.order) {
+					secondExactNumber.mantissa[i] = (byte) (mantissa[i]-numberZeroInBytes);
+				}
+			}
+		}
+		secondExactNumber.order = Math.max(order, secondExactNumber.order)+minusOrder;
 	}
 	
-	public int getSignMantissa() {
-		return signMantissa;
+	public void multiply(ExactNumber secondExactNumber){
+		byte[] middleValue = new byte[30]; 
+		for (int i = mantissa.length; i >= 0 ; i--) {
+			for (int j = mantissa.length; j >= 0; j--) {
+				int productNumber = (mantissa[i]-numberZeroInBytes)*(secondExactNumber.mantissa[j]-numberZeroInBytes);
+				if(productNumber < 10 ) {
+					middleValue[i] =(byte) ((char)productNumber+'0');
+				}else if(productNumber >= 10) {
+					
+				}
+			}
+		}
 	}
 	
-	public void setOrder(int order) {
-		this.order = order;
-	}
-	
-	public void creatArrayMantissa(int lenght) {
-		this.mantissa = new byte[lenght];
-	}
-	
-	public void getArray() {
-		System.out.println(Arrays.toString(mantissa));
-	}
-	
-	public void getFirstFigareMantissa() {
-		System.out.println();
-	}
-	
-	public int setLenght() {
-		return mantissa.length;
+	public String toString() {
+		String number = signMantissa + Arrays.toString(mantissa) + "e" + order;
+		return number;
 	}
 }
--- Menu.java
+++ Menu.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Menu {
 	public static void main(String[] args) {
 		ExactNumber exactNumber = new ExactNumber();
+		ExactNumber exactNumber2 = new ExactNumber();
 		Scanner scanner = new Scanner(System.in);
 		while (true) {
 			try {
@@ -21,19 +22,29 @@ public class Menu {
 				switch (x) {
 				case 1: {
 					inputNumber(exactNumber);
-					exactNumber.getArray();
-					System.out.println(exactNumber.getSignMantissa());
-//					inputNumberWithDecimalPointWhithoutSign(exactNumber, 0);
-//					System.out.println(exactNumber.getOneNumber());
+					System.out.println(exactNumber.toString());
 					break;
 				}
 				case 2:
 					System.out.println();
 					break;
 				case 3:
-					System.out.println(Math.pow(-1, 0));
+					inputNumber(exactNumber);
+					exactNumber.toString();
+					inputNumber(exactNumber2);
+					System.out.println(exactNumber.toString());
+					System.out.println(exactNumber2.toString());
+					exactNumber.addition(exactNumber2);
+					System.out.println(exactNumber2.toString());
 					break;
 				case 4:
+					inputNumber(exactNumber);
+					exactNumber.toString();
+					inputNumber(exactNumber2);
+					System.out.println(exactNumber.toString());
+					System.out.println(exactNumber2.toString());
+					exactNumber.subtraction(exactNumber2);
+					System.out.println(exactNumber2.toString());
 					break;
 				case 5:
 					System.out.println("Завершение работы программы...");
@@ -53,59 +64,18 @@ public class Menu {
 
 	public static void inputNumber(ExactNumber exactNumber) {
 		Scanner scanner = new Scanner(System.in);
-		int minimumAllowedValue = -99999;
-		int maxAllowedValue = 99999;
 		System.out.print("Введите число: ");
 		String stringNumber = scanner.nextLine();
-		char[] charNumber = stringNumber.toCharArray();
-		System.out.println(Arrays.toString(charNumber));
-		exactNumber.creatArrayMantissa(charNumber.length);
-		System.out.println(stringNumber);
-		int index = 0;
-		int order = 0;
-		for (int i = 0; i < charNumber.length; i++) {
-			if (Character.isDigit(charNumber[i])) {
-				exactNumber.setFigureNumber(index, (byte) charNumber[i]);
-				index++;
-			} else if (charNumber[0] == 0 && charNumber[0] == '-') {
-				exactNumber.setSignMantissa(0);
-			} else if (charNumber[0] == 0 && (charNumber[0] == '+' || Character.isDigit(charNumber[0]))) {
-				exactNumber.setSignMantissa(1);
-			} else if (i < 5 && Character.isDigit(charNumber[i])) {
-				order++;
-			} else if (i < 5 && '.' == charNumber[i]) {
-				exactNumber.setOrder(order);
-			} else if (i > 4 && !Character.isDigit(charNumber[i]) && charNumber[i] != 'e') {
-				System.out.println("Программа не работает с числами такого формата");
-			}
-
-		}
-	}
-
-	public static void chooseNumberFormat(ExactNumber exactNumber) {
-		Scanner scanner = new Scanner(System.in);
-		while (true) {
-			System.out.println(
-					"""
-							1)Формат числа с десятичной точкой с знаком или без знака. Пример: 123.13213, -1233.4213, +12323.32131
-							2)Экспоненциа́льный формат записи: 123123e21, 123232e-23, -12323e21
-							3)Экспоненциа́льный формат записи c десятичной точкой: 1232.3121e21
-							""");
-			int x = scanner.nextInt();
-			System.out.println("Выберите формат числа: ");
-			switch (x) {
-			case 1:
-//					inputNumberWithDecimalPointWhithoutSign(exactNumber, 0);
-				break;
-
-			case 2:
-				System.out.println();
-				break;
-			case 3:
-
-			default:
-				System.out.println("Такого формата чисел нет");
-			}
+		int count =  0;
+		int countSign = 0;
+		countSign += stringNumber.length() - stringNumber.replace(String.valueOf('+'), "").length();
+		countSign += stringNumber.length() - stringNumber.replace(String.valueOf('-'), "").length();
+		count += stringNumber.length() - stringNumber.replace(String.valueOf('.'), "").length();
+		count += stringNumber.length() - stringNumber.replace(String.valueOf('e'), "").length();
+		if(stringNumber.matches("[0-9, . , \\+ , \\-, e]+")&&count<=2&&countSign <=2){
+			exactNumber.addNumber(stringNumber);
+		}else {
+			System.out.println("Программа не работает с числами такого формата");
 		}
 	}
 
